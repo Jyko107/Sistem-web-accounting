d7e89b1dc1e078c165e5f1c68ad0659961e55468
Jyko107 <joykoozon@gmail.com>
2024-05-31 00:20:51 +0700

    Update 4_Neraca Saldo.py

--- pages/4_Neraca Saldo.py
+++ pages/4_Neraca Saldo.py
@@ -9,7 +9,7 @@ def skip_bad_lines(bad_lines):
 st.set_page_config(page_title="Neraca Saldo", page_icon=":bar_chart:", layout="wide")
 
 st.sidebar.success("Pilih Menu Diatas ini.")
-
+make_sidebar()
 #header
 with st.container():
     st.title("Laporan Posisi Keuangan")
@@ -20,4 +20,4 @@ with st.container():
     st.subheader ("Neraca Saldo")
     st.dataframe(df, width= 10000, height= 350)    
     st.write("---")
- make_sidebar()   
+ 
